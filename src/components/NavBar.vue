4494bf61fc81035ee055d4a0e6a5b175b461cf4c
ZERGUINE-Nadine <zerguinenadine@gmail.com>
2024-12-19 12:14:16 +0100

    edit NavBare

---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="bg-[#6A4E23] text-[#F5F5DC] p-4 font-cambria">
+      <div class="container mx-auto flex justify-between items-center">
+        <div class="flex items-center">
+          <img src="" alt="BookVerse Logo" class="h-8 mr-4">
+          <a href="../assets/logo.webp" class="text-2xl font-bold text-[#FFD700]">BookVerse</a>
+        </div>
+        <ul class="flex space-x-6">
+          <li>
+            <router-link to="/" class="hover:text-[#FFD700]">Accueil</router-link>
+          </li>
+          <li>
+            <router-link to="/favorites" class="hover:text-[#FFD700]">Favoris</router-link>
+          </li>
+          <li>
+            <router-link to="/login" class="hover:text-[#FFD700]">Connexion</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
+  <script lang="ts">
+  export default {};
+  </script>
+  
+  <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Cambria&display=swap');
+  
+.font-cambria {
+  font-family: 'Cambria', 'Times New Roman', serif;
+}
+
+  </style>
+  
+  
